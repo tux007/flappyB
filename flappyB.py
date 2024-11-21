f354d470a9ef8afb612c5e1a970cab8d2edf3a83
tux007 <thomas@steimann.ch>
2024-11-21 19:21:50 +0100

    "Updated flappyB.py: added import os, modified game over logic, changed bird jump height, and updated pipe texture and creation."

--- flappyB.py
+++ flappyB.py
@@ -1,8 +1,10 @@
-from ursina import * 
+import os
+from ursina import *
 
 app = Ursina()
 Sky()
 bird = Animation('assets/bird_blue', scale=2, collider='box')
+
 
 camera.orthographic = True
 camera.fov = 20
@@ -12,24 +14,29 @@ def update():
     for pipe in pipes:
         pipe.x = pipe.x - 0.05
     if bird.y < -10:
-        quit()
+        Animation('assets/game_over.png', scale=20, position=(1, 1))
+        quit()       
+        
     if bird.y > 10:
+        Animation('assets/game_over.png', scale=20, position=(1, 1))
         quit()
+
     if bird.intersects().hit:
+        Animation('assets/game_over.png', scale=20, position=(1, 1))
         quit()
 
 def input(key):
     if key == 'space':
-        bird.y = bird.y + 3
+        bird.y = bird.y + 2.5
 
 pipes = []
 
 pipe = Entity(model='quad', 
-              color=color.green, 
-              texture='white_cube',
+              texture='assets/pipe',
               position=(20,10),
               scale=(3, 15, 1),
-              collider='box')
+              collider='box'
+              )
 
 def createPipes():
     newY = random.randint(4, 12)
